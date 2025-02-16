3c9ee4a7b71fcfdf8944b5611aa55c8c47026f8d
anton <ashtanyuk@gmail.com>
2025-02-16 20:03:35 +0300

    Множественные исправления

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,20 @@
 
 bool checkPrime(uint64_t value) {
   // вставьте код функции
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
   // вставьте код функции
+  return 2;
 }
 
 uint64_t nextPrime(uint64_t value) {
   // вставьте код функции
+  return 2;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   // вставьте код функции
+  return 2;
 }
